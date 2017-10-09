624e910a56e4c3614703445d553a5be676712e72
tesya <tesyant@gmail.com>
2017-10-09 22:22:33 +0700

    detail

---
+++ app/src/main/java/com/dicoding/tesyant/kamus/DetailIndActivity.java
@@ -0,0 +1,15 @@
+package com.dicoding.tesyant.kamus.model;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.dicoding.tesyant.kamus.R;
+
+public class DetailIndActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_ind);
+    }
+}
